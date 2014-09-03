85120168c1a71591caa757f02ec4b96bd43d080b
Dhanasekaran K P <dhanasekarancs@gmail.com>
2014-09-03 17:03:13 +0530

    Updated Code.

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ public class Tester
 {
   public static void main(String args[])
   {
-    AnagramSolver object = new AnagramsSolver();
+    AnagramSolver object = new AnagramSolver();
     object.hashWords();
 
     object.printAnagrams("makes");
